07806e171aa4e3e75d7227216f578c4e37dced5e
Mamani, Diana <dmamanillanquichoque@santandertecnologia.com.ar>
2021-09-21 01:06:58 -0300

    added header + styles

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
 import './preview-collection.styles.scss';
 
-const PreviewCollection = ({title, items}) => (
-    <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <div className='preview'>
-            {items.map(item => (
-                <div key={item.id}>{item.name}</div>
-            ))}
-        </div>
+const PreviewCollection = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
+  </div>
 );
 
 export default PreviewCollection;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/preview-collection/preview-collection.component'
+import SHOP_DATA from './shop.data.js';
+import PreviewCollection from '../../components/preview-collection/preview-collection.component'
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        };
-
+class ShopPage extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        collections: SHOP_DATA
+      };
     }
-
-    render(){
-        const {collections} = this.state;
-        return ( <div className='shop-page'>
-            {
-                collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key= {id} {...otherCollectionProps}/>
-                ))
-            }
-        </div>);
+  
+    render() {
+      const { collections } = this.state;
+      return (
+        <div className='shop-page'>
+          {collections.map(({ id, ...otherCollectionProps }) => (
+            <PreviewCollection key={id} {...otherCollectionProps} />
+          ))}
+        </div>
+      );
     }
-}
+  }
 
 export default ShopPage;
