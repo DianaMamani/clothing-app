092cfbb4cd5ecabe6b9582755f3608eb04522fe4
Mamani, Diana <dmamanillanquichoque@santandertecnologia.com.ar>
2021-09-26 10:29:09 -0300

    check if the env for the logging

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,13 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger];
+const middlewares = [];
 
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger);
+}
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-const persistor = persistStore(store);
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
 
 export default {store, persistor};
