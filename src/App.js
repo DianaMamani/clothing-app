2b63f33b4baffc140c5d4d431dc6815e1743fd22
Mamani, Diana <dmamanillanquichoque@santandertecnologia.com.ar>
2021-09-25 23:06:14 -0300

    added checkout page and item :9

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {selectCurrentUser} from './redux/user/user.selectors';
-
+import CheckoutPage from './pages/checkout/checkout.component'
 class App extends React.Component{
   unsubscribeFromAuth = null;
 
@@ -45,6 +45,7 @@ class App extends React.Component{
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route exact path='/checkout' component={CheckoutPage}/>
         <Route 
           exact
           path='/signIn' 
