734710dacd9ba657c95a6b089f917e21c6174420
Mamani, Diana <dmamanillanquichoque@santandertecnologia.com.ar>
2021-09-26 01:32:11 -0300

    added stripebase payment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51JdoplChSURVtFqkejisIBmN0TUiIVXPAbqJzC0vnRFnYNyaoJ5X75mZbXfYdnA6T362MbvJySNmSUp1i7XL2uSB00S1ZjN6P6';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Dianas clothing'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
